317ba996fb7499f2b42b8443005f111ce68833eb
Bruno César Vieira Fernandes <bruno.fernandes@estudante.ifgoiano.edu.br>
2022-11-30 10:42:19 -0300

    mais atualizacoes

--- src/main/java/br/com/ifgoiano/trabalhopoo/respositorys/AlunoRepository.java
+++ src/main/java/br/com/ifgoiano/trabalhopoo/respositorys/AlunoRepository.java
@@ -2,10 +2,14 @@ package br.com.ifgoiano.trabalhopoo.respositorys;
 
 import br.com.ifgoiano.trabalhopoo.models.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AlunoRepository  extends JpaRepository<Aluno, String> {
 
     boolean existsByEmail(String email);
 
     boolean existsByIdUser(Long id);
+
+    @Query()
+    String findNameById(Long idUser);
 }
